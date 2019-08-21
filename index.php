79cb2ff471b793a1183d97761611ed78b1d2de97
Razor7w <34554306+Razor7w@users.noreply.github.com>
2019-08-21 17:20:37 -0400

    Hotfix index.php
    Se producía un error al hacer la petición desde IONIC, agregando estas lineas se corrige el problema.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Custom-header");
 
 require __Dir__ . '/bootstrap/app.php';
 
