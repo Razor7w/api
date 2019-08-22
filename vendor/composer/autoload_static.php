688cd543aa3ba75fa530b220b2780fd43178670a
Razor7w <seba.carroza@gmail.com>
2019-08-21 22:31:39 -0400

    Remove illuminate/database

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,17 +7,12 @@ namespace Composer\Autoload;
 class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
 {
     public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
-        '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Symfony\\Component\\Translation\\' => 30,
             'Slim\\' => 5,
         ),
         'P' => 
@@ -32,10 +27,6 @@ class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
         'I' => 
         array (
             'Interop\\Container\\' => 18,
-            'Illuminate\\Support\\' => 19,
-            'Illuminate\\Database\\' => 20,
-            'Illuminate\\Contracts\\' => 21,
-            'Illuminate\\Container\\' => 21,
         ),
         'F' => 
         array (
@@ -48,14 +39,6 @@ class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Mbstring\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Symfony\\Component\\Translation\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/translation',
-        ),
         'Slim\\' => 
         array (
             0 => __DIR__ . '/..' . '/slim/slim/Slim',
@@ -76,22 +59,6 @@ class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
         array (
             0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
         ),
-        'Illuminate\\Support\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/support',
-        ),
-        'Illuminate\\Database\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/database',
-        ),
-        'Illuminate\\Contracts\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/contracts',
-        ),
-        'Illuminate\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/illuminate/container',
-        ),
         'FastRoute\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikic/fast-route/src',
@@ -102,30 +69,12 @@ class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
         ),
     );
 
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/nesbot/carbon/src',
-    );
-
     public static $prefixesPsr0 = array (
-        'U' => 
-        array (
-            'UpdateHelper\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/kylekatarnls/update-helper/src',
-            ),
-        ),
         'P' => 
         array (
             'Pimple' => 
             array (
                 0 => __DIR__ . '/..' . '/pimple/pimple/src',
-            ),
-        ),
-        'D' => 
-        array (
-            'Doctrine\\Common\\Inflector\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/doctrine/inflector/lib',
             ),
         ),
     );
@@ -135,7 +84,6 @@ class ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce::$prefixDirsPsr4;
-            $loader->fallbackDirsPsr4 = ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit9ea693585ecba4633f4370a9cbaea9ce::$prefixesPsr0;
 
         }, null, ClassLoader::class);
